8efe238fbf6104bea348aadf365ff7ed6abed304
Muhammad Abdullah <abdullahprivate2005@gmail.com>
2026-01-15 17:59:03 +0500

    Refactor script.js for navbar and theme functionality

--- script.js
+++ script.js
@@ -237,3 +237,4 @@ ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
 ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
 ScrollReveal().reveal('.home-content p, .about-content p', { origin: 'right' });
+
